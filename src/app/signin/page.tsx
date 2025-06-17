64a7936f30690c73c51854c9d5c7c8b4ff700944
Muhammad-javed440 <meherjaved440@gmail.com>
2025-06-18 02:21:21 +0500

    login

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -120,7 +120,7 @@ function LoginComponent() {
         </div>
         <div className="mt-6">
             <Button
-              onClick={() => signOut({ callbackUrl: "/" })}
+              onClick={() => signOut({ callbackUrl: "/login" })}
               className="w-full bg-indigo-600 text-white hover:bg-indigo-700 dark:hover:bg-indigo-800"
             >
                Logout
