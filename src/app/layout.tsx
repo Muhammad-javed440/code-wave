b77dc9674a0cb4825200f99c3c412cf22957de29
Muhammad-javed440 <meherjaved440@gmail.com>
2025-06-18 03:04:53 +0500

    removed from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+
 import Providers from "@/components/ui/authprovider/Providers";
 
 const geistSans = Geist({
@@ -31,9 +30,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-         <Navbar/>
+        
         {children}
-        <Footer/>
+      
         </Providers>
       </body>
     </html>
